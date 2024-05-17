61698c1777709d0066b7d51d761a5fddc5120e79
Laura Figueroa <laufigueroasalas@gmail.com>
2024-05-16 23:34:01 -0400

    feat: "added equipo section, done by Luz Martinez, modified cards on Servicios section"

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+const masTexto = document.getElementById("masTexto");
+const leerMas = document.getElementsByClassName("btn");
+const enviarForm = document.getElementById("enviarForm");
+
+function mostrar() {
+  var x = document.getElementById("masTexto");
+   if (x.style.display === 'none') {
+       x.style.display = 'block';
+   } else {
+       x.style.display = 'none';
+   }
+}
+
+(function error() { 
+    'use strict';
+    window.addEventListener ('click', function() {
+        // extrae el formulario al que queremos revisar
+        var form = document.getElementsByClassName('needs-validation');
+        // Filtra el form y evita que sea procesado
+        var validation = Array.prototype.filter.call(form, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation(); 
+                }
+                form.classList.add('was-validated');
+            }, false );
+        });
+    }, false);
+})();
